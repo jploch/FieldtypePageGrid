7618b5913d7f36907ff374912479778c1192676c
Jploch <jan.ploch@googlemail.com>
2024-07-25 22:08:46 +0200

    fix for tinyMCE field inline editor causing duplicated content sometimes

--- FieldtypePageGrid.module.php
+++ FieldtypePageGrid.module.php
@@ -16,7 +16,7 @@ class FieldtypePageGrid extends FieldtypeMulti implements Module, ConfigurableMo
     return array(
       'title' => __('PAGEGRID'),
       'summary' => __('Commercial page builder module that renders block templates and adds drag and drop functionality in admin.', __FILE__),
-      'version' => '2.1.26',
+      'version' => '2.1.27',
       'author' => 'Jan Ploch',
       'icon' => 'th',
       'href' => "https://page-grid.com",
