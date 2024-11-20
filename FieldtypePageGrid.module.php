9393906ca9f8d6f94ccbb614758afa533114a09b
Jploch <jan.ploch@googlemail.com>
2024-11-20 14:10:55 +0100

    ui fixes

--- FieldtypePageGrid.module.php
+++ FieldtypePageGrid.module.php
@@ -16,7 +16,7 @@ class FieldtypePageGrid extends FieldtypeMulti implements Module, ConfigurableMo
     return array(
       'title' => __('PAGEGRID'),
       'summary' => __('Commercial page builder module that renders block templates and adds drag and drop functionality in admin.', __FILE__),
-      'version' => '2.1.67',
+      'version' => '2.1.68',
       'author' => 'Jan Ploch',
       'icon' => 'th',
       'href' => "https://page-grid.com",
