3f081454b04b15f136a4711cdffa9b816a338528
Jploch <jan.ploch@googlemail.com>
2025-10-01 19:18:38 +0200

    resize performance improved

--- FieldtypePageGrid.module.php
+++ FieldtypePageGrid.module.php
@@ -16,7 +16,7 @@ class FieldtypePageGrid extends FieldtypeMulti implements Module, ConfigurableMo
     return array(
       'title' => __('PAGEGRID'),
       'summary' => __('A flexible drag-and-drop page builder with exceptional design control.', __FILE__),
-      'version' => '2.2.105',
+      'version' => '2.2.106',
       'author' => 'Jan Ploch',
       'icon' => 'th',
       'href' => "https://page-grid.com",
